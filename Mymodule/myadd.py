0300f30486ae59f23cab6de2c82201e37978768c
Cong_Huy <t20me022@yamanashi.ac.jp>
2023-07-21 22:04:53 +0900

    first

---
+++ Mymodule/myadd.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 21:49:19 2023
+
+@author: konfi
+"""
+import sys
+sys.path.append("C:/Users/konfi/Desktop/Mymodule/x64/Debug")
+import Mymodule
+result = Mymodule.add(4, 3)
+print(result)   
